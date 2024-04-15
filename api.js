ba9d48104a158da8022261829221462e37ea0091
“andrewhaddad“ <andrewghaddad@outlook.com>
2024-04-15 12:52:55 -0400

    Adding /update & /insert endpoints

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('./config');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 
 const corsOptions = {
@@ -11,7 +11,7 @@ const corsOptions = {
 
 const app = express();
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions), express.json())
 const client = new MongoClient(config.uri);
 
 app.get('/', (req, res) => {
@@ -35,6 +35,57 @@ app.get('/mileageCalc/data', async (req, res) => {
     } catch (error) {
         console.error('Error calling POST API:', error);
         res.status(500).json({ error: 'An error occurred while calling the POST API' });
+    }
+});
+
+app.post('/mileageCalc/update', async (req, res) => {
+    try {
+        await client.connect();
+        console.log('Connected successfully to server');
+
+        const db = client.db(config.dbName);
+        const collection = db.collection(config.collection);
+
+        const filter = req.body.filter;
+        const update = req.body.update;
+        
+        // Filter documents and update
+        const result = await collection.updateMany(filter, update);
+
+        console.log('Updated document:');
+        console.log(result);
+
+        return res.json({ result: result });
+    } catch (error) {
+        console.error('Error calling UPDATE API:', error);
+        res.status(500).json({ error: 'An error occurred while calling the UPDATE API' });
+    }
+});
+
+app.post('/mileageCalc/insert', async (req, res) => {
+    try {
+        await client.connect();
+        console.log('Connected successfully to server');
+
+        const db = client.db(config.dbName);
+        const collection = db.collection(config.collection);
+
+        const document = req.body;
+
+        // Creates and assigns new Object Id to request
+        const objectId = new ObjectId()
+        document._id = objectId;
+        
+        // Insert document
+        const result = await collection.insertOne(document);
+
+        console.log('Inserted document:');
+        console.log(result);
+        
+        return res.json({ result: result });
+    } catch (error) {
+        console.error('Error calling INSERT API:', error);
+        res.status(500).json({ error: 'An error occurred while calling the INSERT API' });
     }
 });
 
